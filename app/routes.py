908aed70ca932f20a11c2cecb8aba623377632fd
Daniel Pitulia <daniel@dpitulia.com>
2022-08-26 17:41:52 +0200

    added lots of files, e.g. banner and navbar html to base.html, bootstrap and more

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, url_for
 
-@app.route('/')
 @app.route('/index')
-
+@app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/publictransport')
+def publictransport():
+	return render_template('publictransport.html')
+
+@app.route('/chat')
+def chat():
+	return render_template('chat.html')
+
+@app.route('/unitcircle')
+def chat():
+	return render_template('unitcircle.html')
