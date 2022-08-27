a3dac8d5d75afae3f8aff65fd6bc9e94171a0ce3
Daniel Pitulia <daniel@dpitulia.com>
2022-08-27 13:56:06 +0200

    Written a lot of code in publictransport.py to fetch stop IDs and bus departures. GET and POST requests are working using OAuth2. Here an access code is needed

--- app/publictransport.py
+++ app/publictransport.py
@@ -1,17 +1,58 @@
+# Send request to Västtrafik for 1) access token, 2) bus stop based on input and 3) departures
+
 import base64
 import requests
+import datetime
 
-id = "123"
-nyckel='WO0vwoy1KvBrlheiA97k8XQBOWQa'
-hemlighet='UqBaPs6X2W4ink1FItAtm79XHkUa'
+id = "device_12345"
+nyckel="WO0vwoy1KvBrlheiA97k8XQBOWQa"
+hemlighet="UqBaPs6X2W4ink1FItAtm79XHkUa"
 to_be_encoded = nyckel + ":" + hemlighet
-encoded_string = base64.b64encode(to_be_encoded.encode('ascii')).decode()
+encoded_string = base64.b64encode(to_be_encoded.encode("ascii")).decode()
 
-url = 'https://api.vasttrafik.se/token'
+url_token = "https://api.vasttrafik.se/token"
 data = {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': 'Basic ' + encoded_string + '=grant_type=client_credentials&scope=<' + id + '>'
+    "Content-Type": "application/x-www-form-urlencoded",
+    "Authorization": "Basic " + encoded_string + "=grant_type=client_credentials&scope=" + id,
+    "grant_type": "client_credentials",
+    "client_secret": hemlighet,
+    "client_id": nyckel
 }
 
-x = requests.post(url, data=data)
-print(x.headers)
+x = requests.post(url_token, data=data)
+print(x.json())
+access_token = x.json()['access_token']
+
+# 2) Send request for stop ID based on user input_departure_stop
+
+
+user_input_stopID = "Rävekärrsgatan"
+format = "json"
+url_stopID = "https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=" + user_input_stopID + "&format=" + format
+headers={
+    "Authorization": "Bearer " + access_token
+}
+
+x2 = requests.get(url_stopID, headers=headers)
+print(x2.json()['LocationList']['StopLocation'][0]['name']) # Bus stop name
+stopID = x2.json()['LocationList']['StopLocation'][0]['id']
+
+# 3) Send GET request for departures. This requires some time variables.
+date_now= datetime.datetime.now().strftime("%Y%m%d")
+hour_now= datetime.datetime.now().strftime("%H")
+minutes_now= datetime.datetime.now().strftime("%M")
+url_departures = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=" + stopID + "&date=" + date_now + "&time=" + hour_now + "%3A" + minutes_now + "&format=" + format
+x3 = requests.get(url_departures, headers=headers)
+
+departures = x3.json()['DepartureBoard']['Departure']
+
+for x in departures:
+    date_iso = x['date']
+    time_iso = x['time']
+    dep_date = datetime.datetime.fromisoformat(date_iso + " " + time_iso)
+    current_date = datetime.datetime.now()
+    current_date = current_date.replace(second=0,microsecond=0) # Remove seconds and microseconds for simplicity because they are not provided in the departure times by Västtrafik
+    direction = x['direction']
+    delta = str(int((dep_date - current_date).total_seconds()/60)) # Time difference in minutes
+    vehicle = x['name']
+    print(vehicle + " arrival: " + delta + " min. Departure = " + dep_date.strftime("%H:%M") + " Direction = " + direction)
---
+++ comments.js
@@ -0,0 +1,22 @@
+// {
+//      'name': 'Buss 761',
+//      'sname': '761',
+//      'journeyNumber': '623',
+//      'type': 'BUS',
+//      'stopid': '9022014012312002',
+//      'stop': 'Rävekärrsgatan, Mölndal',
+//      'time': '11:28',
+//      'date': '2022-08-27',
+//      'journeyid': '9015014576100623',
+//      'direction': 'Lindome, Påstigning fram',
+//      'track': 'B',
+//      'rtTime': '11:28',
+//      'rtDate': '2022-08-27',
+//      'fgColor': '#0061EB',
+//      'bgColor': '#FFFF50',
+//      'stroke': 'Solid',
+//      'accessibility': 'wheelChair',
+//      'JourneyDetailRef': {
+//           'ref': 'https://api.vasttrafik.se/bin/rest.exe/v2/journeyDetail?ref=371853%2F142133%2F562968%2F157536%2F80%3Fdate%3D2022-08-27%26station_evaId%3D12312002%26station_type%3Ddep%26format%3Djson%26'
+//      }
+// }
