64d18de87eab0714520cc81d16d2025b7de2bfa5
Daniel Pitulia <daniel@dpitulia.com>
2022-08-28 22:29:16 +0200

    More changes. Front and 3dprint page now has content. Still have problems with 3d_viewer not being smooth in Firefox. Something wrong with mouseover, happens occasionally in my browser for some reason. Footer is still in wrong position

--- app/routes.py
+++ app/routes.py
@@ -11,7 +11,7 @@ token = get_token()
 @app.route('/index')
 @app.route('/')
 def index():
-	return render_template('index.html', title='HOME', date=date)
+	return render_template('index.html', title='HOME', date=date, css="styles")
 
 @app.route('/publictransport', methods=['GET', 'POST'])
 
@@ -20,22 +20,22 @@ def publictransport():
 	if request.method == 'POST':
 		stop_input = request.form['stop_input']
 		departures_zip = get_timetables(token, stop_input)
-		return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip, stop_input = stop_input)
+		return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip, stop_input = stop_input, css="styles_publictransport")
 
 	elif request.method == 'GET':
 		line,delta,destination = "", "", ""
 		departures_zip=zip(line, delta, destination)
 		stop_input=""
-		return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip, stop_input = stop_input)
+		return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip, stop_input = stop_input, css="styles_publictransport")
 
 @app.route('/chat')
 def chat():
-	return render_template('chat.html', title='CHAT', date=date)
+	return render_template('chat.html', title='CHAT', date=date, css="styles_chat")
 
 @app.route('/unitcircle')
 def unitcircle():
-	return render_template('unitcircle.html', title='UNIT CIRCLE', date=date)
+	return render_template('unitcircle.html', title='UNIT CIRCLE', date=date, css="styles_unitcircle")
 
 @app.route('/raspberry')
 def raspberry():
-	return render_template('raspberry.html', title='RASPBERRY', date=date)
+	return render_template('raspberry.html', title='RASPBERRY', date=date, css="styles_3dprint")
