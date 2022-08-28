f2afb4f7d4ed47cbeba106e30dfcc18e935d517e
Daniel Pitulia <daniel@dpitulia.com>
2022-08-28 21:00:59 +0200

    Did some major improvements. The public transport form is now working. The front page is empty and a search generates a departure timetable. Some searches leads to an error, e.g. 'Mölndal centrum'. Will check this later. Also, the token is only generated at the first visit at publictransport.html. So if a user stays for a long time, the token will expire. Might need to look into this.

--- app/routes.py
+++ app/routes.py
@@ -1,20 +1,32 @@
 from app import app
-from flask import render_template, url_for
+from flask import render_template, url_for, request
+from timetables import get_timetables
+from token_vasttrafik import get_token
+
 import datetime
-from publictransport import get_timetables
 
 date = datetime.datetime.now().strftime("%c")
+token = get_token()
 
 @app.route('/index')
 @app.route('/')
 def index():
 	return render_template('index.html', title='HOME', date=date)
 
-@app.route('/publictransport')
+@app.route('/publictransport', methods=['GET', 'POST'])
+
 def publictransport():
 
-	departures_zip = get_timetables("Rävekärr")
-	return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip)
+	if request.method == 'POST':
+		stop_input = request.form['stop_input']
+		departures_zip = get_timetables(token, stop_input)
+		return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip, stop_input = stop_input)
+
+	elif request.method == 'GET':
+		line,delta,destination = "", "", ""
+		departures_zip=zip(line, delta, destination)
+		stop_input=""
+		return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip, stop_input = stop_input)
 
 @app.route('/chat')
 def chat():
---
+++ timetables.py
@@ -0,0 +1,51 @@
+# Send two requests to västtrafik. First, get the ID of the given bus stop. Then, get the departure tables.
+import requests
+import datetime
+
+# 1) Send request for stop ID based on user input_departure_stop
+
+
+
+def get_timetables(token, user_input_stopID):
+    format = "json"
+    url_stopID = "https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=" + user_input_stopID + "&format=" + format
+    headers={
+        "Authorization": "Bearer " + token
+    }
+
+    x2 = requests.get(url_stopID, headers=headers)
+    print(x2.json()['LocationList']['StopLocation'][0]['name']) # Bus stop name
+    stopID = x2.json()['LocationList']['StopLocation'][0]['id']
+
+    # 2) Send GET request for departures. This requires some time variables.
+    date_now= datetime.datetime.now().strftime("%Y%m%d")
+    hour_now= datetime.datetime.now().strftime("%H")
+    minutes_now= datetime.datetime.now().strftime("%M")
+    url_departures = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=" + stopID + "&date=" + date_now + "&time=" + hour_now + "%3A" + minutes_now + "&format=" + format
+    x3 = requests.get(url_departures, headers=headers)
+
+    departures = x3.json()['DepartureBoard']['Departure']
+    current_date = datetime.datetime.now()
+    current_date = current_date.replace(second=0,microsecond=0)
+    # Remove seconds and microseconds for simplicity because they are not provided in the departure times by Västtrafik
+
+    line = list()
+    delta = list()
+    direction = list()
+
+    for x in departures:
+        date_iso = x['date']
+        time_iso = x['time']
+        dep_date = datetime.datetime.fromisoformat(date_iso + " " + time_iso)
+        delta_x = [int((dep_date - current_date).total_seconds()/60)] # Time difference in minutes
+
+        if delta_x[0] > 0:
+            delta += delta_x
+            line += [x['name']]
+            direction += [x['direction']]
+            # Extra brackets converts strings into one list item
+        if len(line) >= 10:
+            break
+
+    departure_zip = zip(line,delta,direction)
+    return departure_zip
