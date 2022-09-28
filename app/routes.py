be0f02d59f7d9b9234a915eb57a35afa0352bd8c
daniel <indigotimberlake@protonmail.com>
2022-09-28 19:28:40 +0200

    removed chat page, public transport page is working well, added margin above footer to create more space

--- app/routes.py
+++ app/routes.py
@@ -8,55 +8,54 @@ import datetime
 
 date = datetime.datetime.now().strftime("%c")
 
+
 @app.route('/index', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	date = datetime.datetime.now().strftime("%c")
-	form = EmailForm()
-	if form.validate_on_submit():
-		mail = Mail(app)
-		msg = Message(form.message.data, sender = form.email.data, recipients = ['daniel@dpitulia.com'])
-		msg.body = form.message.data
-		mail.send(msg)
-		sent_message = "Your message has been sent."
-		return render_template('index.html', title='HOME', date=date, css="styles_index", form=form, sent_message=sent_message)
-	else:
-		return render_template('index.html', title='HOME', date=date, form=form, css="styles_index")
+    date = datetime.datetime.now().strftime("%c")
+    form = EmailForm()
+    if form.validate_on_submit():
+        mail = Mail(app)
+        msg = Message(form.message.data, sender=form.email.data, recipients=['daniel@dpitulia.com'])
+        msg.body = form.message.data
+        mail.send(msg)
+        sent_message = "Your message has been sent."
+        return render_template('index.html', title='HOME', date=date, css="styles_index", form=form, sent_message=sent_message)
+    else:
+        return render_template('index.html', title='HOME', date=date, form=form, css="styles_index")
+
 
 @app.route('/publictransport', methods=['GET', 'POST'])
 def publictransport():
-	form=SearchForm()
-	# Get token from Västtrafik for authentication if it hasn't been done
-	if "token" not in locals():
-		token = get_token()
+    form = SearchForm()
+    # Get token from Västtrafik for authentication if it hasn't been done
+    if "token" not in locals():
+    	token = get_token()
 
-	if form.validate_on_submit():
-		# Get timetables based on user's search
-		date = datetime.datetime.now().strftime("%c")
-		stop_input = form.search.data
-		timetable_and_stop = get_timetables(token, stop_input)
-		departures_zip = timetable_and_stop[0]
-		stop_name = timetable_and_stop[1]
-		return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip, stop_input = stop_input, css="styles_publictransport", stop_name=stop_name, form=form)
-	else:
-		# Render an empty form for first visit
-		line,delta,destination = "", "", ""
-		date = datetime.datetime.now().strftime("%c")
-		departures_zip=zip(line, delta, destination)
-		stop_input=""
-		return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip, stop_input = stop_input, css="styles_publictransport", stop_name="", form=form)
+    if form.validate_on_submit():
+        # Get timetables based on user's search
+        date = datetime.datetime.now().strftime("%c")
+        stop_input = form.search.data
+        timetable_and_stop = get_timetables(token, stop_input)
+        departures_zip = timetable_and_stop[0]
+        stop_name = timetable_and_stop[1]
+        return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip, stop_input=stop_input, css="styles_publictransport", stop_name=stop_name, form=form)
+    else:
+        # Render an empty form for first visit
+        line, delta, destination = "", "", ""
+        date = datetime.datetime.now().strftime("%c")
+        departures_zip = zip(line, delta, destination)
+        stop_input = ""
+        return render_template('publictransport.html', title='TIMETABLE APP', date=date, departures_zip=departures_zip, stop_input=stop_input, css="styles_publictransport", stop_name="", form=form)
 
-@app.route('/chat')
-def chat():
-	date = datetime.datetime.now().strftime("%c")
-	return render_template('chat.html', title='CHAT', date=date, css="styles_chat")
 
 @app.route('/unitcircle')
 def unitcircle():
-	date = datetime.datetime.now().strftime("%c")
-	return render_template('unitcircle.html', title='UNIT CIRCLE', date=date, css="styles_unitcircle", js_online="https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=AM_CHTML", js_static="js/unitcircle/unitcircle.js")
+    date = datetime.datetime.now().strftime("%c")
+    return render_template('unitcircle.html', title='UNIT CIRCLE', date=date, css="styles_unitcircle", js_online="https://cdn.jsdelivr.net/npm/mathjax@2/MathJax.js?config=AM_CHTML", js_static="js/unitcircle/unitcircle.js")
+
 
 @app.route('/raspberry')
 def raspberry():
-	date = datetime.datetime.now().strftime("%c")
-	return render_template('raspberry.html', title='RASPBERRY', date=date, css="styles_3dprint")
+    date = datetime.datetime.now().strftime("%c")
+    return render_template('raspberry.html', title='RASPBERRY', date=date, css="styles_3dprint")
--- timetables.py
+++ timetables.py
@@ -4,10 +4,12 @@ import datetime
 
 # 1) Send request for stop ID based on user input_departure_stop
 
+
 def get_timetables(token, user_input_stopID):
     format = "json"
-    url_stopID = "https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=" + user_input_stopID + "&format=" + format
-    headers={
+    url_stopID = "https://api.vasttrafik.se/bin/rest.exe/v2/location.name?input=" + \
+        user_input_stopID + "&format=" + format
+    headers = {
         "Authorization": "Bearer " + token
     }
 
@@ -15,10 +17,12 @@ def get_timetables(token, user_input_stopID):
     stopID = x2.json()['LocationList']['StopLocation'][0]['id']
 
     # 2) Send GET request for departures. This requires some time variables.
-    date_now= datetime.datetime.now().strftime("%Y%m%d")
-    hour_now= datetime.datetime.now().strftime("%H")
-    minutes_now= datetime.datetime.now().strftime("%M")
-    url_departures = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=" + stopID + "&date=" + date_now + "&time=" + hour_now + "%3A" + minutes_now + "&format=" + format
+    date_now = datetime.datetime.now().strftime("%Y%m%d")
+    hour_now = datetime.datetime.now().strftime("%H")
+    minutes_now = datetime.datetime.now().strftime("%M")
+    url_departures = "https://api.vasttrafik.se/bin/rest.exe/v2/departureBoard?id=" + stopID + \
+        "&date=" + date_now + "&time=" + hour_now + \
+        "%3A" + minutes_now + "&format=" + format
     x3 = requests.get(url_departures, headers=headers)
 
     if x3.json()['DepartureBoard']['Departure']:
@@ -28,19 +32,21 @@ def get_timetables(token, user_input_stopID):
         departures = ["Not found"]
 
     current_date = datetime.datetime.now()
-    current_date = current_date.replace(second=0,microsecond=0)
+    current_date = current_date.replace(second=0, microsecond=0)
     # Remove seconds and microseconds for simplicity because they are not provided in the departure times by Västtrafik
 
     line = list()
     delta = list()
     direction = list()
-    
-    if departures !=  ["Not found"]:
+
+    if departures != ["Not found"]:
         for departure in departures:
             date_iso = departure['date']
             time_iso = departure['time']
-            dep_date = datetime.datetime.fromisoformat(date_iso + " " + time_iso)
-            delta_x = [int((dep_date - current_date).total_seconds()/60)] # Time difference in minutes
+            dep_date = datetime.datetime.fromisoformat(
+                date_iso + " " + time_iso)
+            # Time difference in minutes
+            delta_x = [int((dep_date - current_date).total_seconds() / 60)]
 
             if delta_x[0] > 0:
                 delta += delta_x
@@ -50,7 +56,7 @@ def get_timetables(token, user_input_stopID):
             if len(line) >= 10:
                 break
 
-        departure_zip = zip(line,delta,direction)
+        departure_zip = zip(line, delta, direction)
     else:
         departure_zip = False
 
