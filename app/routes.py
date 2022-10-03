f9dc762084f8df5a372effba4553cef646c4d28a
daniel <indigotimberlake@protonmail.com>
2022-10-03 20:32:36 +0200

    Added tooltips to homepage icons. Didn't get it to work with local bootstrap files so changed to CDN, which solved the problem

--- app/routes.py
+++ app/routes.py
@@ -12,6 +12,7 @@ date = datetime.datetime.now().strftime("%c")
 @app.route('/index', methods=['GET', 'POST'])
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    js_static = "index.js"
     topBanner = "HOME"
     date = datetime.datetime.now().strftime("%c")
     form = EmailForm()
@@ -21,9 +22,9 @@ def index():
         msg.body = form.message.data
         mail.send(msg)
         sent_message = "Your message has been sent."
-        return render_template('index.html', title='HOME - WEBSITE PORTFOLIO', date=date, css="styles_index", form=form, sent_message=sent_message, topBanner = topBanner)
+        return render_template('index.html', title='HOME - WEBSITE PORTFOLIO', date=date, css="styles_index", form=form, sent_message=sent_message, topBanner = topBanner, js_static=js_static)
     else:
-        return render_template('index.html', title='HOME - Web projects', date=date, form=form, css="styles_index", topBanner = topBanner)
+        return render_template('index.html', title='HOME - Web projects', date=date, form=form, css="styles_index", topBanner = topBanner, js_static=js_static)
 
 
 @app.route('/publictransport', methods=['GET', 'POST'])
